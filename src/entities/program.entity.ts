3feed31f32b3471bd0807f0e51116093ed7912c2
Dolphindoor LTD <145857223+dolphindoor@users.noreply.github.com>
2025-09-09 13:05:46 +0000

    quick fix

--- src/entities/program.entity.ts
+++ src/entities/program.entity.ts
@@ -1,13 +1,8 @@
-import { Entity, Column, OneToMany } from 'typeorm';
+import { Entity, Column } from 'typeorm';
 import { BaseModel } from './basemodel.js';
-import { Admission } from './admission.entity.js';
-import { Student } from './student.entity.js';
 
 @Entity('programs')
 export class Program extends BaseModel{
   @Column({ type: 'varchar', length: 255 })
   name?: string;
-
-  @OneToMany(() => Admission, (admission) => admission.program)
-  admissions?: Admission[];
 }
